8ecf7ce0ec0f75492888b4e9c386167c300ad612
Tamani Phiri <tamanigabriel0@gmail.com>
2023-02-21 17:50:33 -0800

    first changes added post and get request

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors"
+
+const app= express();
+const port=5000;
+
+app.use(express.json());
+app.use(cors());
+app.use( express.urlencoded({extended:true}))
+
+const list=[
+    {
+        id:"1",
+        name:"Rabbi",
+        reg:"08"
+    },
+    {
+        id:"2",
+        name:"Tamani",
+        reg:"31"
+    }
+]
+
+app.get('/',(req,res)=>{
+    res.send(list);
+})
+
+app.post('/', (req,res)=>{
+    list.push(req.body);
+    res.send("created");
+});
+
+app.listen(port,()=>{
+    console.log(`app is running at http://localhost:${port}`);
+});
